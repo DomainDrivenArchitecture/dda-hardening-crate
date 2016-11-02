025dd5227fd98ae9823b166982348367fe9f9b85
Jan Krebs <jan.krebs@meissa-gmbh.de>
2016-11-02 14:20:47 +0100

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-hardening-crate "0.1.0"
+(defproject org.domaindrivenarchitecture/dda-hardening-crate "0.1.1-SNAPSHOT"
   :description "The dda collected crate"
   :url "https://www.domaindrivenarchitecture.org"
   :pallet {:source-paths ["src"]}
