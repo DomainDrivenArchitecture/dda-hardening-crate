7ed5949943a19051fcdb8e07f6760632004fbbf7
jem <michael.jerger@meissa-gmbh.de>
2018-03-23 13:02:05 +0100

    applied app-core refactoring

--- main/src/dda/pallet/dda_hardening_crate/app.clj
+++ main/src/dda/pallet/dda_hardening_crate/app.clj
@@ -17,76 +17,42 @@
 (ns dda.pallet.dda-hardening-crate.app
   (:require
    [schema.core :as s]
-   [dda.cm.group :as group]
-   [dda.config.commons.map-utils :as mu]
-   [dda.pallet.commons.secret :as secret]
-   [dda.pallet.commons.existing :as existing]
+   [dda.pallet.core.app :as core-app]
    [dda.pallet.dda-config-crate.infra :as config-crate]
    [dda.pallet.dda-hardening-crate.infra :as infra]
-   [dda.pallet.dda-hardening-crate.domain :as domain]
-   [dda.pallet.commons.external-config :as ext-config]))
+   [dda.pallet.dda-hardening-crate.domain :as domain]))
 
 (def with-hardening infra/with-hardening)
-(def InfraResult domain/InfraResult)
-(def HardeningDomainConfig domain/HardeningDomainConfig)
-(def HardeningResolvedConfig domain/HardeningResolvedConfig)
 
-(def ProvisioningUser existing/ProvisioningUser)
-(def Targets existing/Targets)
+(def InfraResult domain/InfraResult)
+
+(def HardeningDomain domain/HardeningDomain)
 
 (def HardeningAppConfig
   {:group-specific-config
-   {s/Keyword (merge InfraResult)}})
-
-(s/defn ^:always-validate load-targets :- Targets
-  [file-name :- s/Str]
-  (existing/load-targets file-name))
-
-(s/defn ^:always-validate load-domain :- HardeningDomainConfig
-  [file-name :- s/Str]
-  (ext-config/parse-config file-name))
-
-(s/defn dda-hardening-group-spec
-  [app-config :- HardeningAppConfig]
-  (group/group-spec
-    app-config [(config-crate/with-config app-config)
-                with-hardening]))
-
-(s/defn ^:always-validate
-  app-configuration-resolved :- HardeningAppConfig
-  [resolved-domain-config :- HardeningResolvedConfig & options]
-  (let [{:keys [group-key] :or {group-key infra/facility}} options
-        {:keys [type]} resolved-domain-config]
-     {:group-specific-config {group-key (domain/infra-configuration resolved-domain-config)}}))
+   {s/Keyword
+    (merge InfraResult)}})
 
 (s/defn ^:always-validate
   app-configuration :- HardeningAppConfig
-  [domain-config :- HardeningDomainConfig & options]
-  (let [resolved-domain-config (secret/resolve-secrets domain-config HardeningDomainConfig)]
-    (apply app-configuration-resolved resolved-domain-config options)))
+  [domain-config :- HardeningDomain
+   & options]
+  (let [{:keys [group-key]
+         :or  {group-key infra/facility}} options]
+     {:group-specific-config
+      {group-key (domain/infra-configuration domain-config)}}))
 
-(s/defn ^:always-validate
-  existing-provisioning-spec-resolved
-  "Creates an integrated group spec from a domain config and a provisioning user."
-  [domain-config :- HardeningDomainConfig
-   targets-config :- existing/TargetsResolved]
-  (let [{:keys [existing provisioning-user]} targets-config]
-    (merge
-     (dda-hardening-group-spec (app-configuration domain-config))
-     (existing/node-spec provisioning-user))))
+(s/defmethod ^:always-validate
+  core-app/group-spec infra/facility
+  [crate-app
+   domain-config :- HardeningDomain]
+  (let [app-config (app-configuration domain-config)]
+    (core-app/pallet-group-spec)
+    app-config [(config-crate/with-config app-config)
+                with-hardening]))
 
-(s/defn ^:always-validate
-  existing-provisioning-spec
-  "Creates an integrated group spec from a domain config and a provisioning user."
-  [domain-config :- HardeningDomainConfig
-   targets-config :- existing/Targets]
-  (existing-provisioning-spec-resolved domain-config (existing/resolve-targets targets-config)))
-
-(s/defn ^:always-validate existing-provider-resolved
-  [targets-config :- existing/TargetsResolved]
-  (let [{:keys [existing provisioning-user]} targets-config]
-    (existing/provider {:dda-hardening existing})))
-
-(s/defn ^:always-validate existing-provider
-  [targets-config :- existing/Targets]
-  (existing-provider-resolved (existing/resolve-targets targets-config)))
+(def crate-app (core-app/make-dda-crate-app
+                  :facility infra/facility
+                  :domain-schema HardeningDomain
+                  :domain-schema-resolved HardeningDomain
+                  :default-domain-file "hardening.edn"))
