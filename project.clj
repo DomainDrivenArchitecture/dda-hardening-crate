fae76b9f655d23e8d68fdabca2ccba091644d0a4
jem <michael.jerger@meissa-gmbh.de>
2018-06-14 09:13:44 +0200

    Version 1.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-hardening-crate "1.1.0-SNAPSHOT"
+(defproject dda/dda-hardening-crate "1.1.0"
   :description "The dda hardening crate"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
